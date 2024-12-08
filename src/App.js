9171f000cb0da031861c5a6f0e13ac00f3472c49
Gmetisl2 <gmetisl2.ai@gmail.com>
2024-12-08 22:40:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ const AppContainer = styled.div`
   position: relative;
   overflow-x: hidden; /* Prevent horizontal scrolling */
   padding-top: 120px; /* Add padding to the top to avoid overlap */
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.7); /* Adjust transparency here */
+    z-index: 1; /* Ensure the overlay is above the background but below content */
+  }
+  > * {
+    position: relative;
+    z-index: 2; /* Ensure content is above the overlay */
+  }
 `;
 
 function App() {
